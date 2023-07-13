269e300eec3ffaf6d75661bd374c510059494751
shahda shahla <shahla.shahda906@gmail.com>
2023-07-14 00:04:07 +0530

    initialcommit

---
+++ SYLLABUSTRACKERAPP/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import course,day,syllabus,course_syllabus
+
+# Register your models here.
+
+admin.site.register(course)
+admin.site.register(day)
+admin.site.register(syllabus)
+admin.site.register(course_syllabus)
+
+
+
+
+
---
+++ SYLLABUSTRACKERAPP/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path('',views.index),
+    path('course/',views.course),
+    path('coursesyllabus/',views.coursesyllabus),
+    path('day/',views.day),
+    path('syllabus/',views.syllabus),
+ 
+
+    
+]
